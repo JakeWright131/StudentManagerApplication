b7511311ca151a4855304d5ecc69c10ce67dbea9
g00357576 <g00357576@CR1007.gmadtree.gmit.ie>
2019-09-23 13:12:02 +0100

    Changed readme description and added Menu.java

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # StudentManagerApplication
+My name is Jake
--- src/ie/gmit/StudentManager/Main.java
+++ src/ie/gmit/StudentManager/Main.java
@@ -1,7 +1,8 @@
 package ie.gmit.StudentManager;
 
-public class Main {
 
+public class Main {
+    
 	public static void main(String[] args) {
 		
 	}
