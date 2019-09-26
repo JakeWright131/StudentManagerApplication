c91e990a2354510f5c71d47b6f9dfe776f8f8cd7
g00357576 <g00357576@CR56009.gmadtree.gmit.ie>
2019-09-26 16:43:32 +0100

    New features added to Student Manager

--- src/ie/gmit/StudentManager/Main.java
+++ src/ie/gmit/StudentManager/Main.java
@@ -5,13 +5,71 @@ public class Main {
     
 	public static void main(String[] args) {
 		
-	        // Create a new menu object
-	    	Menu menuObj1 = new Menu();
-	    	// Run the start method of the Menu object 
-	    	menuObj1.start();	
+	    // Create a new menu object
+	    Menu menuObj1 = new Menu();
+	    // Run the start method of the Menu object 
+	    menuObj1.start();	
 		
+	    StudentManager sm1 = new StudentManager ();	
+	  
+	    
+	    
+	    
 		Student student1 = new Student("G0022222");
-		System.out.println(student1.getStudentId());
+		/*Student student2 = new Student("G0033333");
+		Student student3 = new Student("G0044444");
+		Student student4 = new Student("G0055555");
+		Student student5 = new Student("G0066666");
+		Student student6 = new Student("G0077777");
+		Student student7 = new Student("G0088888");
+		Student student8 = new Student("G0099999");
+		Student student9 = new Student("G0012121");
+		Student student10 = new Student("G0011111");
+		Student student11 = new Student("G0031313");*/
+		
+		
+		boolean status;
+		int numOfStudents;
+		
+		status = sm1.add(student1);
+		System.out.println(status);
+		
+		/*status = sm1.add(student2);
+		System.out.println(status);
+		
+		status = sm1.add(student3);
+		System.out.println(status);
+		
+		status = sm1.add(student4);
+		System.out.println(status);
+		
+		status = sm1.add(student5);
+		System.out.println(status);
+		
+		status = sm1.add(student6);
+		System.out.println(status);
+		
+		status = sm1.add(student7);
+		System.out.println(status);
+		
+		status = sm1.add(student8);
+		System.out.println(status);
+		
+		status = sm1.add(student9);
+		System.out.println(status);
+		
+		status = sm1.add(student10);
+		System.out.println(status);
+		
+		status = sm1.add(student11);
+		System.out.println(status);
+		
+		/*status = sm1.delete("G0011111");
+		System.out.println(status);*/
+		
+		numOfStudents = sm1.findTotalStudents();
+		System.out.println(numOfStudents);
+		
 		
 	}
 	
--- src/ie/gmit/StudentManager/Student.java
+++ src/ie/gmit/StudentManager/Student.java
@@ -1,66 +1,73 @@
 package ie.gmit.StudentManager;
 
-import java.util.*; // for Date dob
+import java.util.*; // For Date obj
 
-public class Student {
+public class Student{
+	
+    private String studentId;
+    private String firstName;
+    private String surname;
+    private Date dob;
+    private Address address;
+    private Course course;
+    
+    public Student(String studentId){
+        this.studentId = studentId;
+    }
+    
+    public Student(String studentId, String firstName, String surname){
+        this(studentId);
+        this.firstName = firstName;
+        this.surname = surname;
+    }
 
-	//instance variables
-	
-	private String studentId;
-	private String firstName;
-	private String surname;
-	private Course course;
-	private Date dob;
-	private Address address;
-	
-	
-	
-	public Student(String sid) {
-		this.studentId = sid;
-	}
-	
-	
-	public Student(String sid, String fname, String sname, Date dob) {
-		this(sid);
-		this.firstName = fname;
-		this.surname = sname;
-		this.dob = dob;
-		
-	}
-	
-		public String getStudentId() {
+	public String getStudentId() {
 		return studentId;
-		
 	}
-		
-		public void setStudentId(String studentId) {
-			this.studentId = studentId;
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
 	}
-		
-	
-		public String getfirstName() {
-		return this.firstName;
-		
+
+	public String getFirstName() {
+		return firstName;
 	}
-	
-		public String getCourse() {
-		return this.course;
-		
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-	
-	
-	
-	
-	public void setName(String firstname) {
-			this.firstName = firstname;
-			
-	}		
-	
-	public void setCourse(String course) {
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
 		this.course = course;
-	
-	
 	}
-	
 	
 }
+
